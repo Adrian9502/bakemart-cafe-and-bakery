e19f44ff6dcc662be9546743a74915f54c06b781
Adrian <bontojohnadrian@gmail.com>
2025-01-02 20:40:07 +0800

    edit configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-   build: {
-    outDir: 'dist', // Ensure Vite is building to a separate directory
-    rollupOptions: {
-      input: 'src/main.tsx' // Specify the entry point
-    }
+  build: {
+    outDir: 'dist', // Output directory for built files
   },
   plugins: [react()],
-})
+});
